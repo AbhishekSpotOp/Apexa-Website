0e73f76ca02cb80da5a66f345a08d31f169a0458
ABHISHEK SPOT <113419871+AbhishekSpotOp@users.noreply.github.com>
2023-05-08 18:37:37 +0530

    index.jsx
    Signed-off-by: ABHISHEK SPOT <113419871+AbhishekSpotOp@users.noreply.github.com>

--- pages/index.jsx
+++ pages/index.jsx
@@ -10,24 +10,24 @@ export default function Index() {
       <img width="500" src="/img/bck.png" />
       </div>
         <p className="animateHeader text-4xl font-extrabold text-center text-white">
-          Treo
+          Apexa
         </p>
         <p className="animateHeader text-white text-opacity-50 text-center mt-5">
-      Treo is a multipurpose bot for your discord server with Moderation, Fun, Giveaway, Economy, Utility etc... features. 
+      Apexa is a multipurpose bot for your discord server with Moderation, Fun, Economy, Utility, Welcomer etc... features. 
 	  </p>
         <div className="animateHeader mt-10 flex flex-wrap items-center justify-center gap-x-4">
   
-            <Link href={"https://discord.com/api/oauth2/authorize?client_id=1016392200516550736&permissions=2146958591&scope=bot%20applications.commands"}>
+            <Link href={"https://dsc.gg/apexa"}>
               <a
                 className={
                   "flex items-center px-6 justify-center gap-x-2 shadow-lg shadow-amber-600/20 rounded-xl py-4 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white " 
                 }
               >
-          Invite Treo
+          Invite Apexa
               </a>
             </Link>
   <div className="py-10"></div>
-  <Link href={"https://top.gg/bot/1016392200516550736/vote"}>
+  <Link href={"https://top.gg/bot/1003647025096691813/vote"}>
   <a className={" px-6 justify-center gap-x-2 shadow-lg shadow-amber-600/20 rounded-xl py-4 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white " }>
 Vote Treo</a>
   </Link>
@@ -47,8 +47,8 @@ Vote Treo</a>
           <div className="col-span-4">
             <p className="text-white text-4xl font-bold" dangerouslySetInnerHTML={{ __html:"Treo Bot" }} />
             <p className="text-white text-md font-medium text-gray-500/75" dangerouslySetInnerHTML={{ __html: "How about creating the best environment to relax yourself with your own emotions?" }}/>
-            <a href="https://discord.com/oauth2/authorize?client_id=1004699301982773259&permissions=8&scope=bot%20applications.commands" className={"mt-10 flex items-center px-4 justify-center gap-x-2 shadow-lg shadow-amber-600/20 cursor-pointer rounded-xl py-4 font-medium bg-gradient-to-r from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white"}>
-              <i className="fab fa-discord mr-2" />Invite Treo
+            <a href="https://dsc.gg/apexa" className={"mt-10 flex items-center px-4 justify-center gap-x-2 shadow-lg shadow-amber-600/20 cursor-pointer rounded-xl py-4 font-medium bg-gradient-to-r from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white"}>
+              <i className="fab fa-discord mr-2" />Invite Apexa
             </a>
           </div>
 
@@ -63,13 +63,13 @@ Vote Treo</a>
               <div>
                 <div className="text-white">
                   <p className="text-xl font-semibold mt-5"><i className={`fa fa-universal-access text-2xl text-amber-500`} /> Universal</p>
-                  <p className="text-gray-500 line-clamp-4">Treo is a multipurpose bot for your discord server with Moderation, Fun, Giveaway, Economy, Utility etc... features.</p>
+                  <p className="text-gray-500 line-clamp-4">Apexa is a multipurpose bot for your discord server with Moderation, Fun, Welcomer, Economy, Utility etc... features.</p>
                 </div>
               </div>
               <div>
                 <div className="text-white">
                   <p className="text-xl font-semibold mt-5"><i className={`fa fa-shield text-2xl text-amber-500`} /> Security</p>
-                  <p className="text-gray-500 line-clamp-4">An open source safe bot that you can browse and help us develop(Source code soon).</p>
+                  <p className="text-gray-500 line-clamp-4">An safe bot that you can browse and help us develop.</p>
                 </div>
               </div>
               <div>
@@ -87,8 +87,8 @@ Vote Treo</a>
               <>
                 <div className="lg:mx-4 lg:p-8 py-4 lg:py-16 flex items-center justify-center flex-col text-center w-full lg:w-auto lg:text-left lg:flex-none">
                   <img src="https://www.linkpicture.com/q/Capture_449.png"  width="256" className="lg:hidden rounded-xl mb-5 shadow-xl shadow-black" />
-                  <p className="text-3xl text-white font-semibold">Best Your Bot</p>
-                  <p className="text-md text-white font-medium text-gray-500 line-clamp-5">Are you ready to throw the best parties with Treo, the best and most up-to-date bot?</p>
+                  <p className="text-3xl text-white font-semibold">Best Your Apexa</p>
+                  <p className="text-md text-white font-medium text-gray-500 line-clamp-5">Are you ready to throw the best parties with Apexa, the best and most up-to-date bot?</p>
                 </div>
                 <img src="https://www.linkpicture.com/q/Capture_449.png" className="hidden lg:block rounded-xl perspective-right shadow-xl shadow-black" />
               </>
