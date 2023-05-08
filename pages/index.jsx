b1feaa0c668572fe9519c658ba05654bce15e070
ABHISHEK SPOT <113419871+AbhishekSpotOp@users.noreply.github.com>
2023-05-08 21:26:20 +0530

    Update index.jsx
    Signed-off-by: ABHISHEK SPOT <113419871+AbhishekSpotOp@users.noreply.github.com>

--- pages/index.jsx
+++ pages/index.jsx
@@ -86,7 +86,7 @@ Vote Apexa</a>
       <div className="flex flex-row w-full h-auto lg:h-56 my-14 lg:my-40 bg-gradient-to-br lg:px-30 from-neutral-900/90 flex items-center justify-start to-neutral-900/50 rounded-lg p-1 shadow-md">
               <>
                 <div className="lg:mx-4 lg:p-8 py-4 lg:py-16 flex items-center justify-center flex-col text-center w-full lg:w-auto lg:text-left lg:flex-none">
-                  <img src="https://www.linkpicture.com/q/Capture_449.png"  width="256" className="lg:hidden rounded-xl mb-5 shadow-xl shadow-black" />
+                  <img src="https://media.discordapp.net/attachments/1017389436541075520/1105157610690334851/IMG_20230508_211107.jpg"  width="256" className="lg:hidden rounded-xl mb-5 shadow-xl shadow-black" />
                   <p className="text-3xl text-white font-semibold">Best Your Apexa</p>
                   <p className="text-md text-white font-medium text-gray-500 line-clamp-5">Are you ready to throw the best parties with Apexa, the best and most up-to-date bot?</p>
                 </div>
