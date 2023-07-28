a262c80e6d5cbb292fde800d076e81915d8deaea
Abhishek Nigam <113419871+AbhishekSpotOp@users.noreply.github.com>
2023-07-28 13:12:58 +0530

    Update index.jsx
    Signed-off-by: Abhishek Nigam <113419871+AbhishekSpotOp@users.noreply.github.com>

--- pages/index.jsx
+++ pages/index.jsx
@@ -25,6 +25,14 @@ export default function Index() {
               >
           Invite Apexa
               </a>
+		  <Link href={"https://dsc.gg/apexa2"}>
+              <a
+                className={
+                  "flex items-center px-6 justify-center gap-x-2 shadow-lg shadow-amber-600/20 rounded-xl py-4 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white " 
+                }
+              >
+          Invite Apexa 2
+              </a>  
             </Link>
   <div className="py-10"></div>
   <Link href={"https://top.gg/bot/1003647025096691813/vote"}>
