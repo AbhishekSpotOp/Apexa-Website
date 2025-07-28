365ea238fbc933d4f355bf33bfa5d52ec33b09b1
Abhishek Nigam <113419871+AbhishekSpotOp@users.noreply.github.com>
2025-07-28 12:10:49 +0530

    Update partners.jsx
    Signed-off-by: Abhishek Nigam <113419871+AbhishekSpotOp@users.noreply.github.com>

--- pages/partners.jsx
+++ pages/partners.jsx
@@ -18,7 +18,7 @@ export default function Partners() {
             <div className="flex flex-col justify-center text-white rounded">
             <a href="https://discord.gg/z7yD3QT6Xz">
                         <div className="flex-1 gap-x-4 flex items-center bg-gradient-to-b from-neutral-900/80 to-neutral-900/20 p-3 rounded-full">
-                            <img className="rounded-full h-32 w-32" src="https://images-ext-1.discordapp.net/external/Okw6EC0W3xn5-l7wgE-pkMRWkBBVZdxNEP9lOgMlZvY/%3Fsize%3D512/https/cdn.discordapp.com/avatars/885559347818680400/4902b680baedc0a6f1761939e0682894.png" />
+                            <img className="rounded-full h-32 w-32" src="https://cdn.discordapp.com/icons/1270754908894662656/a_b178f7e3c5aa046345561c2d4b80edff.gif?size=1024" />
                             <div>
                                 <h1 className="leading-none text-3xl font-bold text-white">TechStar</h1>        
                                 <div className="flex items-center mt-1">
@@ -30,42 +30,7 @@ export default function Partners() {
                         </div>
                     </a>
                     </div>
-
-                    <div className="flex flex-col justify-center text-white rounded">
-                        <a href="https://discord.gg/JQsGN9wmAp">
-                            <div className="flex-1 gap-x-4 flex items-center bg-gradient-to-b from-neutral-900/80 to-neutral-900/20 p-3 rounded-full">
-                            <img className="rounded-full h-32 w-32" src="https://media.discordapp.net/attachments/1085947319876333728/1155177920072003705/7576949de11f3a0bf80a099f5d26c4f5.png" />
-                            <div>
-                                <h1 className="leading-none text-3xl font-bold text-white">Raiden</h1>        
-                                <div className="flex items-center mt-1">
-                                    <p className="font-normal font-sm">
-                                    Join Raiden Community Today!
-Looking for a place to hang out with gamers and tech enthusiasts from around the world? Look no further than Raiden Community! 
-It is a place where you can hang out with Tech Enthusiasts and Gamers all around the globe.
-                                    </p>
-                                </div>     
-                            </div>
-                        </div>
-                        </a>
-                        </div>
-                <div className="flex flex-col justify-center text-white rounded">
-                        <a href="https://discord.gg/d6rUesGgeX">
-                            <div className="flex-1 gap-x-4 flex items-center bg-gradient-to-b from-neutral-900/80 to-neutral-900/20 p-3 rounded-full">
-                            <img className="rounded-full h-32 w-32" src="https://media.discordapp.net/attachments/1157333816088727613/1157334812978331688/IMG_3296.png?ex=65183b89&is=6516ea09&hm=9504b9488638780913ae9e073dd566412358175ae543bd52c399967395e9b7a6&" />
-                            <div>
-                                <h1 className="leading-none text-3xl font-bold text-white">Digital Agency</h1>        
-                                <div className="flex items-center mt-1">
-                                    <p className="font-normal font-sm">
-                                    🔥  •  Welcome To Digital Agency!
-Hello there! Welcome to my Hangout community server, where you can connect with others, make some new friends, and socialise with other members.
-                                    </p>
-                                </div>     
-                            </div>
-                        </div>
-                        </a>
-                        </div>
-
-                </div>
+                
                 <div className="py-10"></div>
         </>
     );
